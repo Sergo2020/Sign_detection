af16813a7ef48aec46328d65e064db5f0cdefc87
Serge_lab <sergepro2009@gmail.com>
2021-08-10 14:06:19 +0300

    alg added

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from pathlib import Path
-import mesh_utils as ply
+
+import os
+
+import io_utils as ply
+import algorithms as alg
 # https://github.com/falcondai/py-ransac/blob/master/ransac.py
 
-if __name__ == '__main__':
-    obj_path = Path(r"C:\Users\Serge\Desktop\Innoviz\Objects\cube.ply")
-    clster_path = Path(r"C:\Users\Serge\Desktop\Innoviz\Objects\cluster_2.csv")
 
-
+def detect_sign(cluster_file_path: Path):
     points = ply.read_csv(clster_path)
-    ply.show_cluster(points)
+    sign_detector = alg.Sign_Detector()
+    sign_detector.fit_kde(points[:, -1])
+    dens, dens_x = sign_detector.produce_density_arr(points[:, -1], 100, show=True)
+    mode_status = sign_detector.detect_modes(dens_x, dens)
+
+    if mode_status != 1:
+        print('The cluster is not a sign.')
+        os.exit()
+
+
+
+
+if __name__ == '__main__':
+    obj_path = Path(r"Objects\cube.ply")
+    clster_path = Path(r"Objects\cluster_2.csv")
+
+    detect_sign(clster_path)
+
+
